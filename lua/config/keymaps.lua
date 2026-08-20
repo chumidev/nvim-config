4c875ca9f358fcd643df1f94f4f8a6a4e326bcd7
chumidev <chumi.swe@gmail.com>
2026-08-20 10:59:33 +0200

    add keymaps for reordering buffer tabs with Alt+h and Alt+l

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Move current buffer left or right in bufferline
+vim.keymap.set("n", "<A-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
+vim.keymap.set("n", "<A-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })
