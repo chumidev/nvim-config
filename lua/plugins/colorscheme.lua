120292abb3131f995dbffedfe3b9f15852a93811
chumidev <chumi.swe@gmail.com>
2026-08-21 19:14:35 +0200

    change line number above and below for tokyonight theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,5 +4,9 @@ return {
   opts = {
     style = "moon",
     transparent = true,
+    on_highlights = function(hl, c)
+      hl.LineNrAbove = { fg = "#8C5E3C" }
+      hl.LineNrBelow = { fg = "#8C5E3C" }
+    end,
   },
 }
